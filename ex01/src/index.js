ffa6724b7b341666587458c1bdc465acf1f1405b
AnelB <anel.b@live.com>
2022-04-01 18:09:55 +0200

    prvo ubacivanje

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,1 +1,16 @@
+function main(){
+    // Only change code below this line
+    var myFirstName = "Anel";
+    var myLastName = "Babic";
+    var myHomeTown = "Sarajevo"
+    // Only change code above this line
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    }; 
+}
+
+console.log(main());	
+module.experts = main;
 
