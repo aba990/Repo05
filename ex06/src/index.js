8605cacad099c09714fe3e706ed5bc221ef131ad
AnelB <anel.b@live.com>
2022-04-01 22:26:20 +0200

    sesto ubacivanje

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -1,9 +1,9 @@
 function main() {
     // Only change code below this line
-    myString = "Concatenation is the process of appending ";
+    var myString = "Concatenation is the process of appending ";
      myString += "one string to the end of another string";
 // Only change code above this line
     return myString;
 }	
 console.log(main());	
-module.experts = main;
+module.exports = main;
