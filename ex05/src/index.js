01b4a5e8c449ae413209591e56e36de187457463
AnelB <anel.b@live.com>
2022-04-01 18:14:51 +0200

    peto ubacivanje

---
+++ ex05/src/index.js
@@ -0,0 +1,8 @@
+function main() {
+    // Only change code below this line
+    var myString = "This is the first string. " + "This is second string.";
+    // Only change code above this line
+    return myString;
+}	
+console.log(main());	
+module.experts = main;
