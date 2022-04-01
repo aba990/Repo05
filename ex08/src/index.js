20593e34bb69b52dfa63cc58b03b7e38c2837f80
AnelB <anel.b@live.com>
2022-04-01 19:52:58 +0200

    osmo ubacivanje

---
+++ ex08/src/index.js
@@ -0,0 +1,10 @@
+function main() {
+    // Only change code below this line
+    someAdjective = "the best!";
+    myString = "Mistral is ";
+    myString += someAdjective;
+// Only change code above this line
+    return myString;
+}	
+console.log(main());	
+module.experts = main;
