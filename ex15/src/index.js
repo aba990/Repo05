c085fbbe2b949fe42365bfdc85137cc4e2e8b7f3
AnelB <anel.b@live.com>
2022-04-01 23:07:42 +0200

    petnaesto ubacivanje

---
+++ ex15/src/index.js
@@ -0,0 +1,12 @@
+function main() {
+    // Only change code below this code
+    var myNoun = "dog";
+    var myVerb = "ran";
+    var myAdjective = "big";
+    var myAdverb = "quickly";
+    var wordBlanks = "I have a " + myNoun + " who can " + myVerb + " and is " + myAdjective + " as he is " + myAdverb + "."; 
+    // Only change code above this code
+    return wordBlanks;
+}
+console.log(main());
+module.exports = main;
